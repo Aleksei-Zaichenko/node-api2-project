bd95cd3563431f2a33f608153555809313973fc9
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-07 19:09:42 -0400

    got all three GET requests working

--- data/routers/router.js
+++ data/routers/router.js
@@ -21,32 +21,37 @@ router.get('/', (req, res) => {
   router.get('/:id', (req, res) => {
     Lambda.findById(req.params.id)
     .then(post => {
-      if (post) {
+      if (post.length > 0) {
+        console.log(post);
         res.status(200).json(post);
       } else {
-        res.status(404).json({ message: 'Hub not found' });
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
       }
     })
     .catch(error => {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error retrieving the hub',
+        message: 'The post information could not be retrieved.',
       });
     });
   });
   
-  // router.get('/:id/messages', (req, res) => {
-  //   Hubs.findHubMessages(req.params.id)
-  //   .then((messages) => {
-  //     res.status(201).json(messages);
-  //   })
-  //   .catch(error => {
-  //     // log error to database
-  //     console.log(error);
-  //     res.status(500).json({message: 'Error adding the hub'});
-  //   });
-  // });
+  router.get('/:id/comments', (req, res) => {
+    Lambda.findPostComments(req.params.id)
+    .then((posts) => {
+      if(posts.length > 0){
+          res.status(201).json(posts);
+      } else {
+        res.status(404).json({message: 'The posts with the specified ID does not exist.'});
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({message: 'The comments information could not be retrieved.'});
+    });
+  });
 
   // router.post('/', (req, res) => {
   //   Hubs.add(req.body)
