aa0672976ccb395e107caca46c33461abb606bbd
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-07 20:29:32 -0400

    completed all posts requests

--- data/routers/router.js
+++ data/routers/router.js
@@ -5,68 +5,106 @@ const Lambda = require('../db.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Lambda.find(req.query)
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(error => {
-      // log error to database
-      console.log(error);
-      res.status(500).json({
-        error: 'The posts information could not be retrieved.'
-      });
+  Lambda.find(req.query)
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      error: 'The posts information could not be retrieved.'
     });
   });
+});
   
-  router.get('/:id', (req, res) => {
-    Lambda.findById(req.params.id)
-    .then(post => {
-      if (post.length > 0) {
-        console.log(post);
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
-      }
-    })
-    .catch(error => {
-      // log error to database
-      console.log(error);
-      res.status(500).json({
-        message: 'The post information could not be retrieved.',
-      });
+router.get('/:id', (req, res) => {
+  Lambda.findById(req.params.id)
+  .then(post => {
+    if (post.length > 0) {
+      console.log(post);
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'The post information could not be retrieved.',
     });
   });
+});
   
-  router.get('/:id/comments', (req, res) => {
-    Lambda.findPostComments(req.params.id)
-    .then((posts) => {
-      if(posts.length > 0){
-          res.status(201).json(posts);
-      } else {
-        res.status(404).json({message: 'The posts with the specified ID does not exist.'});
-      }
-    })
-    .catch(error => {
-      // log error to database
-      console.log(error);
-      res.status(500).json({message: 'The comments information could not be retrieved.'});
-    });
+router.get('/:id/comments', (req, res) => {
+  Lambda.findPostComments(req.params.id)
+  .then((posts) => {
+    if(posts.length > 0){
+        res.status(201).json(posts);
+    } else {
+      res.status(404).json({message: 'The posts with the specified ID does not exist.'});
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({message: 'The comments information could not be retrieved.'});
   });
+});
 
-  // router.post('/', (req, res) => {
-  //   Hubs.add(req.body)
-  //   .then(hub => {
-  //     res.status(201).json(hub);
-  //   })
-  //   .catch(error => {
-  //     // log error to database
-  //     console.log(error);
-  //     res.status(500).json({
-  //       message: 'Error adding the hub',
-  //     });
-  //   });
-  // });
+router.post('/', (req, res) => {
+  if(req.body.title && req.body.contents){
+    Lambda.insert(req.body)
+    .then(newPost => {
+      if(newPost){
+        res.status(201).json(newPost);
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json(
+        {
+          error: "There was an error while saving the post to the database",
+        });
+    });
+  } else {
+      res.status(500).json({
+          errorMessage: "Please provide title and contents for the post.",
+      });
+  }
+});
   
+router.post('/:id/comments', (req, res) => {
+  Lambda.findById(req.params.id)
+  .then(post => {
+    if (post.length > 0) {
+      if(req.body.text){
+        Lambda.insertComment(req.body)
+        .then(newComment =>{
+          console.log(newComment);
+          res.status(201).json({newComment});
+        })
+        .catch(err => {
+          res.status(500),json({error: "The posts information could not be retrieved."})
+        })
+      } else {
+        res.status(400).json({errorMessage: "Please provide text for the comment."})
+      }
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(404).json({
+      message: "The post with the specified ID does not exist.",
+    });
+  });  
+});
+
   // router.delete('/:id', (req, res) => {
   //   Hubs.remove(req.params.id)
   //   .then(count => {
@@ -106,20 +144,6 @@ router.get('/', (req, res) => {
   //     console.log(error);
   //     res.status(500).json({
   //       message: 'Error updating the hub',
-  //     });
-  //   });
-  // });
-
-  // router.post('/:id/messages', (req, res) => {
-  //   Hubs.addMessage(req.body)
-  //       .then(message => {
-  //           res.status(201).json(message);
-  //   })
-  //   .catch(error => {
-  //     // log error to database
-  //     console.log(error);
-  //     res.status(500).json({
-  //       message: 'Error adding the hub',
   //     });
   //   });
   // });
